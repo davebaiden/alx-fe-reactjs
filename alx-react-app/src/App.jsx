a296962b3301cd4fffa422e44266985023e761dd
David Baiden Amissah <davidbamissah@gmail.com>
2025-08-31 20:06:58 +0000

    Test

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
+import UserProfile from './UserProfile'
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
         <MainContent />
         <Footer />
         <WelcomeMessage />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> 
       </div>
+      
   )
 }
-
 export default App
