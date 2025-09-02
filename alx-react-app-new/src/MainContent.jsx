6b92eac3d9c8ba65694857c02b10007a55807534
David Baiden Amissah <davidbamissah@gmail.com>
2025-09-02 16:24:49 +0000

    Experiment on react

---
+++ alx-react-app-new/src/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent(){
+    return (
+        <main style={{fontWeight: 'bold', fontFamily: 'cursive'}}>
+           <p style={{color: 'blue'}}>I love to visit New York, Paris, and Tokyo.</p>
+           <p>I am experimenting on something new.</p>
+        </main>
+    );
+}
+
+export default MainContent;
