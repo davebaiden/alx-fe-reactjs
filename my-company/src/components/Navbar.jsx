9a501298a1a24e5eba589420e66d7298330add49
David Baiden Amissah <davidbamissah@gmail.com>
2025-09-07 21:38:30 +0000

    n

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#333", color: "#fff" }}>
-      <Link to="/" style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}>Home</Link>
-      <Link to="/about" style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}>About</Link>
-      <Link to="/services" style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}>Services</Link>
-      <Link to="/contact" style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}>Contact</Link>
+    <nav
+      style={{
+        padding: "10px",
+        backgroundColor: "#333",
+        display: "flex",
+        justifyContent: "center",
+        gap: "15px",
+      }}
+    >
+      <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>Home</Link>
+      <Link to="/about" style={{ color: "#fff", textDecoration: "none" }}>About</Link>
+      <Link to="/services" style={{ color: "#fff", textDecoration: "none" }}>Services</Link>
+      <Link to="/contact" style={{ color: "#fff", textDecoration: "none" }}>Contact</Link>
     </nav>
   );
 }
