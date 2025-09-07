87eef90068537234ef09ab3d3fa9242f704ecdbe
David Baiden Amissah <davidbamissah@gmail.com>
2025-09-07 09:50:20 +0000

    edit App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
 import UserProfile from './UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Footer />
         <WelcomeMessage />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> 
+        <Counter />
       </div>
       
   )
