8d19ac527387c8edadb8850159cc6277b0b2674d
David Baiden Amissah <davidbamissah@gmail.com>
2025-09-07 21:16:08 +0000

    Edit UserProfile

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,11 +1,17 @@
-const UserProfile = (props) => {
-   return (
-     <div>
-       <h2>{props.name}</h2>
-       <p>Age: {props.age}</p>
-       <p>Bio: {props.bio}</p>
-     </div>
-   );
- };
+// src/UserProfile.jsx
+import { useContext } from "react";
+import UserContext from "../UserContext";
 
- export default UserProfile
+const UserProfile = () => {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
